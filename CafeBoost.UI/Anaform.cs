67d8d1a69d14dfc8265cc83cd7787f75510384c0
Onur Öztürk <onurozturk_97@hotmail.com>
2020-12-01 17:51:48 +0300

    Masa Tasima changes

--- CafeBoost.UI/Anaform.cs
+++ CafeBoost.UI/Anaform.cs
@@ -67,6 +67,7 @@ namespace CafeBoost.UI
                 siparis = new Siparis();
                 siparis.MasaNo = masaNo;
                 db.Siparisler.Add(siparis);
+                db.SaveChanges();
                 lvwMasalar.SelectedItems[0].ImageKey = "dolu";
             }
 
