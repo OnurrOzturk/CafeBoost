4fb1f63bc449ceb42e86226acd5219d8677d4e59
Onur Öztürk <onurozturk_97@hotmail.com>
2020-12-01 11:36:27 +0300

    ef installed

--- CafeBoost.UI/Anaform.Designer.cs
+++ CafeBoost.UI/Anaform.Designer.cs
@@ -38,46 +38,52 @@
             // lvwMasalar
             // 
             this.lvwMasalar.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lvwMasalar.Location = new System.Drawing.Point(0, 24);
+            this.lvwMasalar.HideSelection = false;
+            this.lvwMasalar.Location = new System.Drawing.Point(0, 60);
+            this.lvwMasalar.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.lvwMasalar.Name = "lvwMasalar";
-            this.lvwMasalar.Size = new System.Drawing.Size(557, 489);
+            this.lvwMasalar.Size = new System.Drawing.Size(1485, 1163);
             this.lvwMasalar.TabIndex = 0;
             this.lvwMasalar.UseCompatibleStateImageBehavior = false;
             this.lvwMasalar.DoubleClick += new System.EventHandler(this.lvwMasalar_DoubleClick);
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiUrunler,
             this.tsmiGecmisSiparisler});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(557, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(16, 5, 0, 5);
+            this.menuStrip1.Size = new System.Drawing.Size(1485, 60);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // tsmiUrunler
             // 
             this.tsmiUrunler.Name = "tsmiUrunler";
-            this.tsmiUrunler.Size = new System.Drawing.Size(58, 20);
+            this.tsmiUrunler.Size = new System.Drawing.Size(140, 50);
             this.tsmiUrunler.Text = "Ürünler";
             this.tsmiUrunler.Click += new System.EventHandler(this.tsmiUrunler_Click);
             // 
             // tsmiGecmisSiparisler
             // 
             this.tsmiGecmisSiparisler.Name = "tsmiGecmisSiparisler";
-            this.tsmiGecmisSiparisler.Size = new System.Drawing.Size(108, 20);
+            this.tsmiGecmisSiparisler.Size = new System.Drawing.Size(266, 50);
             this.tsmiGecmisSiparisler.Text = "Geçmiş Şiparişler";
             this.tsmiGecmisSiparisler.Click += new System.EventHandler(this.tsmiGecmisSiparisler_Click);
             // 
             // Anaform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 513);
+            this.ClientSize = new System.Drawing.Size(1485, 1223);
             this.Controls.Add(this.lvwMasalar);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Anaform";
             this.Text = "Cafe Boost";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Anaform_FormClosing_1);
